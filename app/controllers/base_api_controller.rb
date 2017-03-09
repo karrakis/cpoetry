a8a1892a70d9d6f671703e18b51413ff1d7437e3
ghuestis <george.huestis@locallabs.com>
2017-03-09 12:12:02 -0600

    rawr

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -1,33 +1,14 @@
 class BaseApiController < ApplicationController
-   before_action :parse_request, :authenticate_user_from_token!
-   logger.debug("what the ever-living fuck 0")
-   logger.debug(@json)
-   logger.debug("at least it's talking now")
-   private
-   def authenticate_user_from_token!
-    logger.debug(@json)
-    logger.debug("what the ever-living fuck 2")
-    puts @json
-    puts "why the ever-living fuck"
-     if !@json['api_token']
-       render nothing: true, status: :unauthorized
-     else
-       @user = nil
-       User.find_each do |u|
-         logger.debug(@json)
-         logger.debug(u)
-         if Devise.secure_compare(u.api_token, @json['api_token'])
-           @user = u
-         end
-       end
-     end
-   end
+  before_action :parse_request, :authenticate
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      @user = User.where(api_key: token).first
+    end
+  end
 
    def parse_request
-     logger.debug("what the ever-living fuck x")
-     puts "why the ever-living fuck"
      @json = JSON.parse(request.body.read)
-     logger.debug("what the ever-living fuck 1")
-     puts "why the ever-living fuck"
    end
 end
