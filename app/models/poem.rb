46d99af4cfa5f01e5777b187beadea157191f8cf
ghuestis <george.huestis@locallabs.com>
2017-03-12 00:23:30 -0600

    rawr

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,10 +1,10 @@
 class Poem < ApplicationRecord
-	def self.write_poem
+	def write_poem
 		@words = Word.all.sample(250).map{|m| m.as_json.merge(syllables: syllables(m['word']))}
 		return @words.first
 	end
 
-	def self.syllables(word)
+	def syllables(word)
 		word.scan(/([AEIOUaeiouy]+(?!$|s$|d$))|((?<=k|v|z)[AEIOUaeiouy]+)|(ly$)|(ing$)|((?:p|b)le$)|([^AEIOUY]e(?:s|d)$)/).count
 	end
 end
