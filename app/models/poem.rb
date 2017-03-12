034540b7c43d3e98683bdf6c0dd1f401b833ead5
ghuestis <george.huestis@locallabs.com>
2017-03-12 00:59:36 -0600

    rawr

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -3,8 +3,49 @@ class Poem < ApplicationRecord
 	end
 
 	def self.write_poem
-		@words = Word.all.sample(500).reject{|m| m['word'].match(/[^a-z]/) }.map{|m| m.as_json.merge(syllables: self.syllables(m['word']))}
-		return @words.first
+		@words = Word.all.sample(500).reject{|m| m['word'].match(/[^a-z]/) }.map{|m| m.as_json.merge(syllables: self.syllables(m['word'])) }
+		@words = @words.map{|m| m['syllables'] > 0 ? m : m.merge({syllables: 1})}
+		
+		sorted = {}
+
+		@words.each do |hash|
+			sorted[hash['syllables']] ||= []
+			sorted[hash['syllables']] << hash['word']
+		end
+
+		if !sorted.has_key?(1)
+			sorted[1] = ['a','the','of','and']
+		end
+
+		lines = [5, 7, 5]
+
+		output = []
+
+		lines.each do |syllables|
+			invalid = 1
+			while invalid
+				line = ''
+				while syllables > 0
+					temp = sorted.reject{|k,v| k > syllables}
+
+					syls = temp.keys[randomize.rand(temp.keys.count)]
+					
+					wurdz = temp[syls]
+					
+					line = line + ' ' + wurdz[randomize.rand(wurdz.count)]
+					
+					syllables -= syls
+				end
+
+				analysis = Nokogiri::XML("<sentence>" + tgr.add_tags(line) + "</sentence>")
+				if analysis.xpath('//nn') != [] and analysis.xpath('//vbz') != []
+					invalid = nil
+				end
+			end
+			output << line.strip.capitalize
+		end
+
+		output.join("<br>")
 	end
 
 	def self.syllables(word)
