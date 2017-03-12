99656f851185f5d8d2e8ab54c14654bb7c8b4def
ghuestis <george.huestis@locallabs.com>
2017-03-12 00:20:12 -0600

    rawr

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,6 +1,6 @@
 class Poem < ApplicationRecord
 	def self.write_poem
-		@words = Word.all.sample(250).map{|m| m.to_h.merge(syllables: syllables(m['word']))}
+		@words = Word.all.sample(250).map{|m| m.as_json.merge(syllables: syllables(m['word']))}
 		return @words.first
 	end
 
